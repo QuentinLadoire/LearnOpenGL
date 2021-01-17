321a54f06bfeca001d49720a72d1f46a36dd5ece
Ladoire Zaghet <quentinladoire@gmail.com>
2021-01-17 17:58:29 +0100

    Create and Display a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,64 @@
 
+#include <iostream>
+
 #include <glad/glad.h>
+#include <glfw-3-3-2/GLFW/glfw3.h>
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
 int main()
 {
+	//Window initialization
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); in Mac Os X
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to Create GLFW window" << std::endl;
+		glfwTerminate();
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//Glad initialization
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//Viewport and Resize Callback Initialization
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+
+	//Render Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		//Input
+		ProcessInput(window);
+
+		//Rendering commands
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Check and Call events and Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
