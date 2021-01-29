09f22029a1acdbf39da574221e51d1b1d72885b0
Ladoire Zaghet <quentinladoire@gmail.com>
2021-01-29 20:41:44 +0100

    Add id version of SetUniforms in Shader

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -10,49 +10,83 @@ class Shader
 
 	void Use() const;
 
+	void SetBool(int id, bool value) const;
 	void SetBool(const std::string& name, bool value) const;
 
+	void SetFloat(const int id, float value) const;
 	void SetFloat(const std::string& name, float value) const;
+	void Set2Float(const int id, float value1, float value2) const;
 	void Set2Float(const std::string& name, float value1, float value2) const;
+	void Set3Float(const int id, float value1, float value2, float value3) const;
 	void Set3Float(const std::string& name, float value1, float value2, float value3) const;
+	void Set4Float(const int id, float value1, float value2, float value3, float value4) const;
 	void Set4Float(const std::string& name, float value1, float value2, float value3, float value4) const;
 
+	void SetInt(const int id, int value) const;
 	void SetInt(const std::string& name, int value) const;
+	void Set2Int(const int id, int value1, int value2) const;
 	void Set2Int(const std::string& name, int value1, int value2) const;
+	void Set3Int(const int id, int value1, int value2, int value3) const;
 	void Set3Int(const std::string& name, int value1, int value2, int value3) const;
+	void Set4Int(const int id, int value1, int value2, int value3, int value4) const;
 	void Set4Int(const std::string& name, int value1, int value2, int value3, int value4) const;
 
+	void SetUInt(const int id, unsigned int value) const;
 	void SetUInt(const std::string& name, unsigned int value) const;
+	void Set2UInt(const int id, unsigned int value1, unsigned int value2) const;
 	void Set2UInt(const std::string& name, unsigned int value1, unsigned int value2) const;
+	void Set3UInt(const int id, unsigned int value1, unsigned int value2, unsigned int value3) const;
 	void Set3UInt(const std::string& name, unsigned int value1, unsigned int value2, unsigned int value3) const;
+	void Set4UInt(const int id, unsigned int value1, unsigned int value2, unsigned int value3, unsigned int value4) const;
 	void Set4UInt(const std::string& name, unsigned int value1, unsigned int value2, unsigned int value3, unsigned int value4) const;
 	
+	void SetFloatArray(const int id,  int count, const float* value) const;
 	void SetFloatArray(const std::string& name,  int count, const float* value) const;
+	void Set2FloatArray(const int id, int count, const float* value) const;
 	void Set2FloatArray(const std::string& name, int count, const float* value) const;
+	void Set3FloatArray(const int id, int count, const float* value) const;
 	void Set3FloatArray(const std::string& name, int count, const float* value) const;
+	void Set4FloatArray(const int id, int count, const float* value) const;
 	void Set4FloatArray(const std::string& name, int count, const float* value) const;
 
+	void SetIntArray(const int id, int count, const int* value) const;
 	void SetIntArray(const std::string& name, int count, const int* value) const;
+	void Set2IntArray(const int id, int count, const int* value) const;
 	void Set2IntArray(const std::string& name, int count, const int* value) const;
+	void Set3IntArray(const int id, int count, const int* value) const;
 	void Set3IntArray(const std::string& name, int count, const int* value) const;
+	void Set4IntArray(const int id, int count, const int* value) const;
 	void Set4IntArray(const std::string& name, int count, const int* value) const;
 
+	void SetUIntArray(const int id, int count, const unsigned int* value) const;
 	void SetUIntArray(const std::string& name, int count, const unsigned int* value) const;
+	void Set2UIntArray(const int id, int count, const unsigned int* value) const;
 	void Set2UIntArray(const std::string& name, int count, const unsigned int* value) const;
+	void Set3UIntArray(const int id, int count, const unsigned int* value) const;
 	void Set3UIntArray(const std::string& name, int count, const unsigned int* value) const;
+	void Set4UIntArray(const int id, int count, const unsigned int* value) const;
 	void Set4UIntArray(const std::string& name, int count, const unsigned int* value) const;
 
+	void SetMatrix2(const int id, int count, bool transpose, const float* value);
 	void SetMatrix2(const std::string& name, int count, bool transpose, const float* value);
+	void SetMatrix3(const int id, int count, bool transpose, const float* value);
 	void SetMatrix3(const std::string& name, int count, bool transpose, const float* value);
+	void SetMatrix4(const int id, int count, bool transpose, const float* value);
 	void SetMatrix4(const std::string& name, int count, bool transpose, const float* value);
 
+	void SetMatrix2x3(const int id, int count, bool transpose, const float* value);
 	void SetMatrix2x3(const std::string& name, int count, bool transpose, const float* value);
+	void SetMatrix3x2(const int id, int count, bool transpose, const float* value);
 	void SetMatrix3x2(const std::string& name, int count, bool transpose, const float* value);
 
+	void SetMatrix2x4(const int id, int count, bool transpose, const float* value);
 	void SetMatrix2x4(const std::string& name, int count, bool transpose, const float* value);
+	void SetMatrix4x2(const int id, int count, bool transpose, const float* value);
 	void SetMatrix4x2(const std::string& name, int count, bool transpose, const float* value);
 
+	void SetMatrix3x4(const int id, int count, bool transpose, const float* value);
 	void SetMatrix3x4(const std::string& name, int count, bool transpose, const float* value);
+	void SetMatrix4x3(const int id, int count, bool transpose, const float* value);
 	void SetMatrix4x3(const std::string& name, int count, bool transpose, const float* value);
 
 	const unsigned int GetId() const;
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -93,26 +93,51 @@ void Shader::Use() const
 	glUseProgram(m_id);
 }
 
+void Shader::SetBool(const int id, bool value) const
+{
+	glUseProgram(m_id);
+	glUniform1i(id, (int)value);
+}
 void Shader::SetBool(const std::string& name, bool value) const
 {
 	glUseProgram(m_id);
 	glUniform1i(glGetUniformLocation(m_id, name.c_str()), (int)value);
 }
 
+void Shader::SetFloat(const int id, float value) const
+{
+	glUseProgram(m_id);
+	glUniform1f(id, value);
+}
 void Shader::SetFloat(const std::string& name, float value) const
 {
 	glUseProgram(m_id);
 	glUniform1f(glGetUniformLocation(m_id, name.c_str()), value);
+}
+void Shader::Set2Float(const int id, float value1, float value2) const
+{
+	glUseProgram(m_id);
+	glUniform2f(id, value1, value2);
 }
 void Shader::Set2Float(const std::string& name, float value1, float value2) const
 {
 	glUseProgram(m_id);
 	glUniform2f(glGetUniformLocation(m_id, name.c_str()), value1, value2);
 }
+void Shader::Set3Float(const int id, float value1, float value2, float value3) const
+{
+	glUseProgram(m_id);
+	glUniform3f(id, value1, value2, value3);
+}
 void Shader::Set3Float(const std::string& name, float value1, float value2, float value3) const
 {
 	glUseProgram(m_id);
 	glUniform3f(glGetUniformLocation(m_id, name.c_str()), value1, value2, value3);
+}
+void Shader::Set4Float(const int id, float value1, float value2, float value3, float value4) const
+{
+	glUseProgram(m_id);
+	glUniform4f(id, value1, value2, value3, value4);
 }
 void Shader::Set4Float(const std::string& name, float value1, float value2, float value3, float value4) const
 {
@@ -120,20 +145,40 @@ void Shader::Set4Float(const std::string& name, float value1, float value2, floa
 	glUniform4f(glGetUniformLocation(m_id, name.c_str()), value1, value2, value3, value4);
 }
 
+void Shader::SetInt(const int id, int value) const
+{
+	glUseProgram(m_id);
+	glUniform1i(id, value);
+}
 void Shader::SetInt(const std::string& name, int value) const
 {
 	glUseProgram(m_id);
 	glUniform1i(glGetUniformLocation(m_id, name.c_str()), value);
+}
+void Shader::Set2Int(const int id, int value1, int value2) const
+{
+	glUseProgram(m_id);
+	glUniform2i(id, value1, value2);
 }
 void Shader::Set2Int(const std::string& name, int value1, int value2) const
 {
 	glUseProgram(m_id);
 	glUniform2i(glGetUniformLocation(m_id, name.c_str()), value1, value2);
 }
+void Shader::Set3Int(const int id, int value1, int value2, int value3) const
+{
+	glUseProgram(m_id);
+	glUniform3i(id, value1, value2, value3);
+}
 void Shader::Set3Int(const std::string& name, int value1, int value2, int value3) const
 {
 	glUseProgram(m_id);
 	glUniform3i(glGetUniformLocation(m_id, name.c_str()), value1, value2, value3);
+}
+void Shader::Set4Int(const int id, int value1, int value2, int value3, int value4) const
+{
+	glUseProgram(m_id);
+	glUniform4i(id, value1, value2, value3, value4);
 }
 void Shader::Set4Int(const std::string& name, int value1, int value2, int value3, int value4) const
 {
@@ -141,20 +186,40 @@ void Shader::Set4Int(const std::string& name, int value1, int value2, int value3
 	glUniform4i(glGetUniformLocation(m_id, name.c_str()), value1, value2, value3, value4);
 }
 
+void Shader::SetUInt(const int id, unsigned int value) const
+{
+	glUseProgram(m_id);
+	glUniform1ui(id, value);
+}
 void Shader::SetUInt(const std::string& name, unsigned int value) const
 {
 	glUseProgram(m_id);
 	glUniform1ui(glGetUniformLocation(m_id, name.c_str()), value);
+}
+void Shader::Set2UInt(const int id, unsigned int value1, unsigned int value2) const
+{
+	glUseProgram(m_id);
+	glUniform2ui(id, value1, value2);
 }
 void Shader::Set2UInt(const std::string& name, unsigned int value1, unsigned int value2) const
 {
 	glUseProgram(m_id);
 	glUniform2ui(glGetUniformLocation(m_id, name.c_str()), value1, value2);
 }
+void Shader::Set3UInt(const int id, unsigned int value1, unsigned int value2, unsigned int value3) const
+{
+	glUseProgram(m_id);
+	glUniform3ui(id, value1, value2, value3);
+}
 void Shader::Set3UInt(const std::string& name, unsigned int value1, unsigned int value2, unsigned int value3) const
 {
 	glUseProgram(m_id);
 	glUniform3ui(glGetUniformLocation(m_id, name.c_str()), value1, value2, value3);
+}
+void Shader::Set4UInt(const int id, unsigned int value1, unsigned int value2, unsigned int value3, unsigned int value4) const
+{
+	glUseProgram(m_id);
+	glUniform4ui(id, value1, value2, value3, value4);
 }
 void Shader::Set4UInt(const std::string& name, unsigned int value1, unsigned int value2, unsigned int value3, unsigned int value4) const
 {
@@ -162,20 +227,40 @@ void Shader::Set4UInt(const std::string& name, unsigned int value1, unsigned int
 	glUniform4ui(glGetUniformLocation(m_id, name.c_str()), value1, value2, value3, value4);
 }
 
+void Shader::SetFloatArray(const int id, int count, const float* value) const
+{
+	glUseProgram(m_id);
+	glUniform1fv(id, count, value);
+}
 void Shader::SetFloatArray(const std::string& name, int count, const float* value) const
 {
 	glUseProgram(m_id);
 	glUniform1fv(glGetUniformLocation(m_id, name.c_str()), count, value);
+}
+void Shader::Set2FloatArray(const int id, int count, const float* value) const
+{
+	glUseProgram(m_id);
+	glUniform2fv(id, count, value);
 }
 void Shader::Set2FloatArray(const std::string& name, int count, const float* value) const
 {
 	glUseProgram(m_id);
 	glUniform2fv(glGetUniformLocation(m_id, name.c_str()), count, value);
 }
+void Shader::Set3FloatArray(const int id, int count, const float* value) const
+{
+	glUseProgram(m_id);
+	glUniform3fv(id, count, value);
+}
 void Shader::Set3FloatArray(const std::string& name, int count, const float* value) const
 {
 	glUseProgram(m_id);
 	glUniform3fv(glGetUniformLocation(m_id, name.c_str()), count, value);
+}
+void Shader::Set4FloatArray(const int id, int count, const float* value) const
+{
+	glUseProgram(m_id);
+	glUniform4fv(id, count, value);
 }
 void Shader::Set4FloatArray(const std::string& name, int count, const float* value) const
 {
@@ -183,20 +268,40 @@ void Shader::Set4FloatArray(const std::string& name, int count, const float* val
 	glUniform4fv(glGetUniformLocation(m_id, name.c_str()), count, value);
 }
 
+void Shader::SetIntArray(const int id, int count, const int* value) const
+{
+	glUseProgram(m_id);
+	glUniform1iv(id, count, value);
+}
 void Shader::SetIntArray(const std::string& name, int count, const int* value) const
 {
 	glUseProgram(m_id);
 	glUniform1iv(glGetUniformLocation(m_id, name.c_str()), count, value);
+}
+void Shader::Set2IntArray(const int id, int count, const int* value) const
+{
+	glUseProgram(m_id);
+	glUniform2iv(id, count, value);
 }
 void Shader::Set2IntArray(const std::string& name, int count, const int* value) const
 {
 	glUseProgram(m_id);
 	glUniform2iv(glGetUniformLocation(m_id, name.c_str()), count, value);
 }
+void Shader::Set3IntArray(const int id, int count, const int* value) const
+{
+	glUseProgram(m_id);
+	glUniform3iv(id, count, value);
+}
 void Shader::Set3IntArray(const std::string& name, int count, const int* value) const
 {
 	glUseProgram(m_id);
 	glUniform3iv(glGetUniformLocation(m_id, name.c_str()), count, value);
+}
+void Shader::Set4IntArray(const int id, int count, const int* value) const
+{
+	glUseProgram(m_id);
+	glUniform4iv(id, count, value);
 }
 void Shader::Set4IntArray(const std::string& name, int count, const int* value) const
 {
@@ -204,20 +309,40 @@ void Shader::Set4IntArray(const std::string& name, int count, const int* value) 
 	glUniform4iv(glGetUniformLocation(m_id, name.c_str()), count, value);
 }
 
+void Shader::SetUIntArray(const int id, int count, const unsigned int* value) const
+{
+	glUseProgram(m_id);
+	glUniform1uiv(id, count, value);
+}
 void Shader::SetUIntArray(const std::string& name, int count, const unsigned int* value) const
 {
 	glUseProgram(m_id);
 	glUniform1uiv(glGetUniformLocation(m_id, name.c_str()), count, value);
+}
+void Shader::Set2UIntArray(const int id, int count, const unsigned int* value) const
+{
+	glUseProgram(m_id);
+	glUniform2uiv(id, count, value);
 }
 void Shader::Set2UIntArray(const std::string& name, int count, const unsigned int* value) const
 {
 	glUseProgram(m_id);
 	glUniform2uiv(glGetUniformLocation(m_id, name.c_str()), count, value);
 }
+void Shader::Set3UIntArray(const int id, int count, const unsigned int* value) const
+{
+	glUseProgram(m_id);
+	glUniform3uiv(id, count, value);
+}
 void Shader::Set3UIntArray(const std::string& name, int count, const unsigned int* value) const
 {
 	glUseProgram(m_id);
 	glUniform3uiv(glGetUniformLocation(m_id, name.c_str()), count, value);
+}
+void Shader::Set4UIntArray(const int id, int count, const unsigned int* value) const
+{
+	glUseProgram(m_id);
+	glUniform4uiv(id, count, value);
 }
 void Shader::Set4UIntArray(const std::string& name, int count, const unsigned int* value) const
 {
@@ -225,15 +350,30 @@ void Shader::Set4UIntArray(const std::string& name, int count, const unsigned in
 	glUniform4uiv(glGetUniformLocation(m_id, name.c_str()), count, value);
 }
 
+void Shader::SetMatrix2(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix2fv(id, count, transpose, value);
+}
 void Shader::SetMatrix2(const std::string& name, int count, bool transpose, const float* value)
 {
 	glUseProgram(m_id);
 	glUniformMatrix2fv(glGetUniformLocation(m_id, name.c_str()), count, transpose, value);
 }
+void Shader::SetMatrix3(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix3fv(id, count, transpose, value);
+}
 void Shader::SetMatrix3(const std::string& name, int count, bool transpose, const float* value)
 {
 	glUseProgram(m_id);
 	glUniformMatrix3fv(glGetUniformLocation(m_id, name.c_str()), count, transpose, value);
+}
+void Shader::SetMatrix4(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix4fv(id, count, transpose, value);
 }
 void Shader::SetMatrix4(const std::string& name, int count, bool transpose, const float* value)
 {
@@ -241,10 +381,20 @@ void Shader::SetMatrix4(const std::string& name, int count, bool transpose, cons
 	glUniformMatrix4fv(glGetUniformLocation(m_id, name.c_str()), count, transpose, value);
 }
 
+void Shader::SetMatrix2x3(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix2x3fv(id, count, transpose, value);
+}
 void Shader::SetMatrix2x3(const std::string& name, int count, bool transpose, const float* value)
 {
 	glUseProgram(m_id);
 	glUniformMatrix2x3fv(glGetUniformLocation(m_id, name.c_str()), count, transpose, value);
+}
+void Shader::SetMatrix3x2(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix3x2fv(id, count, transpose, value);
 }
 void Shader::SetMatrix3x2(const std::string& name, int count, bool transpose, const float* value)
 {
@@ -252,10 +402,20 @@ void Shader::SetMatrix3x2(const std::string& name, int count, bool transpose, co
 	glUniformMatrix3x2fv(glGetUniformLocation(m_id, name.c_str()), count, transpose, value);
 }
 
+void Shader::SetMatrix2x4(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix2x4fv(id, count, transpose, value);
+}
 void Shader::SetMatrix2x4(const std::string& name, int count, bool transpose, const float* value)
 {
 	glUseProgram(m_id);
 	glUniformMatrix2x4fv(glGetUniformLocation(m_id, name.c_str()), count, transpose, value);
+}
+void Shader::SetMatrix4x2(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix4x2fv(id, count, transpose, value);
 }
 void Shader::SetMatrix4x2(const std::string& name, int count, bool transpose, const float* value)
 {
@@ -263,10 +423,20 @@ void Shader::SetMatrix4x2(const std::string& name, int count, bool transpose, co
 	glUniformMatrix4x2fv(glGetUniformLocation(m_id, name.c_str()), count, transpose, value);
 }
 
+void Shader::SetMatrix3x4(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix3x4fv(id, count, transpose, value);
+}
 void Shader::SetMatrix3x4(const std::string& name, int count, bool transpose, const float* value)
 {
 	glUseProgram(m_id);
 	glUniformMatrix3x4fv(glGetUniformLocation(m_id, name.c_str()), count, transpose, value);
+}
+void Shader::SetMatrix4x3(const int id, int count, bool transpose, const float* value)
+{
+	glUseProgram(m_id);
+	glUniformMatrix4x3fv(id, count, transpose, value);
 }
 void Shader::SetMatrix4x3(const std::string& name, int count, bool transpose, const float* value)
 {
